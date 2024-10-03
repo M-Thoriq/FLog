493fee76817d181419c5822b47965fbc4d76e7f2
M-Thoriq <mthoriqalasyjari@gmail.com>
2024-10-03 10:41:02 +0700

    Initialize Project

---
+++ app/src/main/java/com/thoriq/flog/MainActivity.kt
@@ -0,0 +1,173 @@
+package com.thoriq.flog
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.thoriq.flog.ui.screen.HomeScreen
+import com.thoriq.flog.ui.theme.FLogTheme
+
+data class TabBarItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val badgeAmount: Int? = null
+)
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            val homeTab = TabBarItem(title = "Home", selectedIcon = Icons.Filled.Home, unselectedIcon = Icons.Outlined.Home)
+            val fishesTab = TabBarItem(title = "Alerts", selectedIcon = Icons.Filled.Notifications, unselectedIcon = Icons.Outlined.Notifications)
+            val settingsTab = TabBarItem(title = "Settings", selectedIcon = Icons.Filled.Settings, unselectedIcon = Icons.Outlined.Settings)
+            val accountTab = TabBarItem(title = "More", selectedIcon = Icons.Filled.List, unselectedIcon = Icons.Outlined.List)
+//            val scanTab
+            
+            // creating a list of all the tabs
+            val tabBarItems = listOf(homeTab, fishesTab, settingsTab, accountTab)
+
+            // creating our navController
+            val navController = rememberNavController()
+
+            FLogTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold(bottomBar = { TabView(tabBarItems, navController) }) {
+                        NavHost(navController = navController, startDestination = homeTab.title) {
+                            composable(homeTab.title) {
+                                HomeScreen()
+                            }
+                            composable(fishesTab.title) {
+                                Text(fishesTab.title)
+//                                TODO("Tambahin Alert()")
+                            }
+                            composable(settingsTab.title) {
+                                Text(settingsTab.title)
+//                                TODO("Tambahin HomeScreen()")
+                            }
+                            composable(accountTab.title) {
+                                MoreView()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TabView(tabBarItems: List<TabBarItem>, navController: NavController) {
+    var selectedTabIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    NavigationBar {
+        tabBarItems.forEachIndexed { index, tabBarItem ->
+            NavigationBarItem(
+                selected = selectedTabIndex == index,
+                onClick = {
+                    selectedTabIndex = index
+                    navController.navigate(tabBarItem.title)
+                },
+                icon = {
+                    TabBarIconView(
+                        isSelected = selectedTabIndex == index,
+                        selectedIcon = tabBarItem.selectedIcon,
+                        unselectedIcon = tabBarItem.unselectedIcon,
+                        title = tabBarItem.title,
+                        badgeAmount = tabBarItem.badgeAmount
+                    )
+                },
+                label = {Text(tabBarItem.title)})
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TabBarIconView(
+    isSelected: Boolean,
+    selectedIcon: ImageVector,
+    unselectedIcon: ImageVector,
+    title: String,
+    badgeAmount: Int? = null
+) {
+    BadgedBox(badge = { TabBarBadgeView(badgeAmount) }) {
+        Icon(
+            imageVector = if (isSelected) {selectedIcon} else {unselectedIcon},
+            contentDescription = title
+        )
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun TabBarBadgeView(count: Int? = null) {
+    if (count != null) {
+        Badge {
+            Text(count.toString())
+        }
+    }
+}
+
+@Composable
+fun MoreView() {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text("Thing 1")
+        Text("Thing 2")
+        Text("Thing 3")
+        Text("Thing 4")
+        Text("Thing 5")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FLogTheme {
+        HomeScreen()
+    }
+}
---
+++ app/src/main/java/com/thoriq/flog/ui/screen/HomeScreen.kt
@@ -0,0 +1,99 @@
+package com.thoriq.flog.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.thoriq.flog.R
+import com.thoriq.flog.ui.theme.BlueF
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
+    ) {
+        GreetingSection()
+        WeatherCard()
+    }
+}
+
+@Composable
+fun GreetingSection(modifier: Modifier = Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(18.dp)
+    ) {
+        Box(modifier = Modifier.padding(8.dp)){
+            Image(painter = painterResource(id = R.drawable.akun), contentDescription = "Image Akun",
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                )
+        }
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(horizontal = 15.dp)
+        ) {
+            Text(text = "Good Morning", fontSize = 24.sp)
+            Text(text = "Have a nice day!")
+        }
+    }
+}
+
+@Composable
+fun WeatherCard(modifier: Modifier = Modifier) {
+    val degree = "23" + "°C"
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(horizontal = 20.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .fillMaxWidth()
+            .background(BlueF)
+            .padding(vertical=24.dp, horizontal=36.dp)
+    ) {
+        Text(degree, fontSize = 48.sp, color = Color.White, fontWeight = FontWeight.ExtraBold)
+    }
+}
+
+@Preview
+@Composable
+private fun HomeScreenPrev() {
+    HomeScreen()
+}
